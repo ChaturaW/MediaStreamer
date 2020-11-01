97cd46ac6df9dbc49233bb97b9395c64c8b4a8fc
Chatura Wijetunga <chaturawijetunga@gmail.com>
2020-11-01 22:36:30 +0530

    Added the search box

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <header>
-            <Link to={"/"}>
-                <img className="logo" src="/Logo.png" alt=""/>                
-            </Link>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-4">
+                        <Link to={"/"}>
+                            <img className="logo" src="/Logo.png" alt="" />
+                        </Link>
+                    </div>
+                    <div className="col-8">
+                        <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
+                    </div>
+                </div>
+            </div>
         </header>
     );
 }
